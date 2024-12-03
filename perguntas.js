2698df9e945177ed80fb96736067c2d72b934f92
Yaslira24 <yasmin.lira.duarte@escola.pr.gov.br>
2024-12-02 21:10:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual é o elemento químico representado pelo símbolo O?',
+    'Oxigênio'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual país é conhecido como a terra do sol nascente?',
+    'Japão'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quantos segundos há em uma hora?',
+    '3600 segundos'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quem pintou a obra A Última Ceia?',
+    'Leonardo da Vinci'
+)
